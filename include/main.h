7b7f9c0044ca3e31343493511bd3e5d73a539d17
stefan <stefan.naeve@hotmail.com>
2024-11-03 13:09:58 +0100

    Now correctly reads the first chunk in PNG.

--- include/main.h
+++ include/main.h
@@ -1,4 +1,30 @@
 #ifndef PNGREADER_MAIN_H
 #define PNGREADER_MAIN_H
 
+typedef struct _IHDR_DATA {
+    uint32_t width;
+    uint32_t height;
+    uint8_t bitDepth;
+    uint8_t colorType;
+    uint8_t compressionMethod;
+    uint8_t filterMethod;
+    uint8_t interlaceMethod;
+} IHDR_DATA;
+
+typedef struct _IHDR_HEADER {
+    uint32_t length;
+    uint32_t chunkType;
+    IHDR_DATA chunkData;
+    uint32_t CRC;
+} IHDR_HEADER;
+
+typedef struct _PNG_HEADER {
+    uint8_t highBitSet;
+    uint8_t PNG[3];
+    uint16_t DOS;
+    uint8_t idfk;
+    uint8_t idkman;
+    IHDR_HEADER ihdr;
+} PNG_HEADER;
+
 #endif //PNGREADER_MAIN_H
--- src/main.c
+++ src/main.c
@@ -2,23 +2,19 @@
 #include <stdint.h>
 #include <malloc.h>
 #include "../include/main.h"
+
+#include <sys/types.h>
+
 #include "../include/SNLogger.h"
 
-typedef struct _IHDR {
-    uint32_t length;
-    uint32_t chunkType;
-    uint8_t *chunkData;
-    uint32_t CRC;
-} IHDR;
+typedef enum _BIT_CHANGE {
+    sixteen,
+    thirtyTwo
+} BIT_CHANGE;
 
-typedef struct _PNG_HEADER {
-    uint8_t highBitSet;
-    uint8_t PNG[3];
-    uint16_t DOS;
-    uint8_t idfk;
-    uint8_t idkman;
-    IHDR ihdr;
-} PNG_HEADER;
+void changeEndian(void *pointer, BIT_CHANGE bitChange);
+
+
 
 int main(void) {
     char theChar;
@@ -34,50 +30,40 @@ int main(void) {
     }
 
     fseek(png, 0, SEEK_END);
-    long thesize = ftell(png);
-    printf("Size: %ld\n", thesize);
     fseek(png, 0, SEEK_SET);
 
     PNG_HEADER pngHeader;
-    printf("Position highBitSet: %ld\n", ftell(png));
     fread(&pngHeader.highBitSet, sizeof(uint8_t), 1, png);
-    printf("Position PNG: %ld\n", ftell(png));
     fread(&pngHeader.PNG, sizeof(uint8_t), 3, png);
-    printf("Position DOS: %ld\n", ftell(png));
     fread(&pngHeader.DOS, sizeof(uint16_t), 1, png);
-    printf("Position idfk: %ld\n", ftell(png));
     fread(&pngHeader.idfk, sizeof(uint8_t), 1, png);
-    printf("Position idkman: %ld\n", ftell(png));
     fread(&pngHeader.idkman, sizeof(uint8_t), 1, png);
-    printf("Position length: %ld\n", ftell(png));
 
     uint32_t temp;
     fread(&temp, sizeof(uint32_t), 1, png);
-    printf("Position: %ld\n", ftell(png));
 
-    // 1010 0010 0011 1100 0101 1010 0010 0011
-    // 1100 0101 0000 0000 0000 0000 0000 0000
-    // AND
-    // 1111 1111 0000 0000 0000 0000 0000 0000
+    changeEndian(&temp, thirtyTwo);
 
-    printf("%u\n", temp);
+    printf("Swapped: %u\n", temp);
 
-    uint32_t swapped = ((temp<<24&0xff000000) |
-                        (temp<<8&0xff0000) |
-                        (temp>>8&0xff00) |
-                        (temp>>24&0xff));
-
-    printf("Swapped: %u\n", swapped);
-
-
-    pngHeader.ihdr.length = swapped;
+    pngHeader.ihdr.length = temp;
 
     fread(&pngHeader.ihdr.chunkType, sizeof(uint32_t), 1, png);
 
-    pngHeader.ihdr.chunkData = calloc(pngHeader.ihdr.length, sizeof(uint8_t));
+    fread(&pngHeader.ihdr.chunkData.width, sizeof(uint32_t), 1, png);
+    changeEndian(&pngHeader.ihdr.chunkData.width, thirtyTwo);
 
-    fread(&pngHeader.ihdr.chunkData, sizeof(uint8_t), pngHeader.ihdr.length, png);
+    fread(&pngHeader.ihdr.chunkData.height, sizeof(uint32_t), 1, png);
+    changeEndian(&pngHeader.ihdr.chunkData.height, thirtyTwo);
+
+    fread(&pngHeader.ihdr.chunkData.bitDepth, sizeof(uint8_t), 1, png);
+    fread(&pngHeader.ihdr.chunkData.colorType, sizeof(uint8_t), 1, png);
+    fread(&pngHeader.ihdr.chunkData.compressionMethod, sizeof(uint8_t), 1, png);
+    fread(&pngHeader.ihdr.chunkData.filterMethod, sizeof(uint8_t), 1, png);
+    fread(&pngHeader.ihdr.chunkData.interlaceMethod, sizeof(uint8_t), 1, png);
+
     fread(&pngHeader.ihdr.CRC, sizeof(uint32_t), 1, png);
+    changeEndian(&pngHeader.ihdr.CRC, thirtyTwo);
 
     printf("highBitSet: %d\n", pngHeader.highBitSet);
     printf("PNG: %c%c%c\n", pngHeader.PNG[0], pngHeader.PNG[1], pngHeader.PNG[2]);
@@ -86,18 +72,44 @@ int main(void) {
     printf("Idfkman: %d\n", pngHeader.idkman);
     printf("Length of data: %d\n", pngHeader.ihdr.length);
 
-    uint32_t swappeded;
+    uint8_t chunkTypeOne = pngHeader.ihdr.chunkType >> 0;
+    uint8_t chunkTypeTwo = pngHeader.ihdr.chunkType >> 8 & 0xff;
+    uint8_t chunkTypeThree = pngHeader.ihdr.chunkType >> 16 & 0xff;
+    uint8_t chunkTypeFour = pngHeader.ihdr.chunkType >> 24 & 0xff;
 
+    printf("Chunk type: %c%c%c%c\n", chunkTypeOne, chunkTypeTwo, chunkTypeThree, chunkTypeFour);
 
-    char newchar[5];
-    newchar[0] = pngHeader.ihdr.chunkType << 24;
-    newchar[1] = pngHeader.ihdr.chunkType << 16;
-    newchar[2] = pngHeader.ihdr.chunkType << 8;
-    newchar[3] = pngHeader.ihdr.chunkType << 0;
-    newchar[4] = '\0';
-    printf("ChunkType: %s\n", newchar);
+    printf("\nChunk data:\n");
+    printf("\tWidth: %d\n", pngHeader.ihdr.chunkData.width);
+    printf("\tHeight: %d\n", pngHeader.ihdr.chunkData.height);
+    printf("\tBit depth: %d\n", pngHeader.ihdr.chunkData.bitDepth);
+    printf("\tColor type: %d\n", pngHeader.ihdr.chunkData.colorType);
+    printf("\tCompression method: %d\n", pngHeader.ihdr.chunkData.compressionMethod);
+    printf("\tFilter method: %d\n", pngHeader.ihdr.chunkData.filterMethod);
+    printf("\tInterlace method: %d\n", pngHeader.ihdr.chunkData.interlaceMethod);
 
-    printf("Chunk data: %s", pngHeader.ihdr.chunkData);
-    printf("CRC: %d", pngHeader.ihdr.CRC);
+    printf("\nCRC: %08X\n", pngHeader.ihdr.CRC);
+    fclose(png);
+}
 
+void changeEndian(void *pointer, BIT_CHANGE bitChange) {
+    uint16_t *temp16;
+    uint32_t *temp32;
+    switch (bitChange) {
+        case sixteen:
+            temp16 = (uint16_t *) pointer;
+            *temp16 = ((*temp16 << 8) |
+                       (*temp16 >> 8));
+            break;
+        case thirtyTwo:
+            temp32 = pointer;
+            *temp32 = ((*temp32 << 24 & 0xff000000) |
+                       (*temp32 << 8 & 0xff0000) |
+                       (*temp32 >> 8 & 0xff00) |
+                       (*temp32 >> 24 & 0xff));
+            break;
+        default:
+            printf("Something went wrong\n");
+            break;
+    }
 }
